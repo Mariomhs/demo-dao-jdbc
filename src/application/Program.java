fbd1eff46f6c2fb763c1f8d7d5915bd947521e18
Mario Souza <mario.mhsouza@gmail.com>
2022-02-08 01:14:30 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Mario", "mario@gmail.com", new Date(30/04/1985), 3000.00, obj);
+		System.out.println(seller);
 
 	}
 
